aa52a23f65cb8f549613a58fab47b4bf374a43da
Alex Brickwedde <alex_github@brickwedde.de>
2022-03-03 13:58:45 +0000

    x

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 "use strict";
 
+const mqtt = require('mqtt')
+const mqttclient  = mqtt.connect('mqtt://ralph.nfsroot.de')
+
 var ModbusRTU   = require ("modbus-serial");
 var client      = new ModbusRTU();
 
@@ -11,15 +14,20 @@ client.on("error", function(error) {
 
     client.setTimeout (250);
 
+function doModbus() {
     client.connectRTUBuffered ("/dev/ttyUSB0", { baudRate: 2400, parity: "none", dataBits: 8, stopBits: 1 })
         .then(async function()
         {
-            console.log([await readModbusData(1), await readModbusData(2), await readModbusData(3)])
+            await readModbusData(1)
+            await readModbusData(2)
+            await readModbusData(3)
+	setTimeout(() => {process.exit(0)}, 1000)
         })
         .catch(function(e)
         {
+process.exit(1)
         });
-
+}
 
 var readModbusData = async function(id)
 {
@@ -34,7 +42,19 @@ const response = await client.poll({
     maxChunkSize: 32,  // max registers per request
     skipErrors: false, // if false it will stop poll and return PARTIAL result
 })
+if(response.error) {
+  await mqttclient.publish('modbus/'+id+'/error', JSON.stringify(response))
+} else {
+ for(var o of response.data) {
+  await mqttclient.publish('modbus/'+id+'/'+o.address, JSON.stringify(o.value))
+ }
+}
 return response
 };
 
+
+mqttclient.on('connect', () => {
+  mqttclient.publish('modbus/conn',""+new Date().getTime())
+  doModbus();
+})
 
