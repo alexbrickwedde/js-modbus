fb6ed8944e24965acfc50798b179e76104a19c5b
alex@brickwedde.de <alex@brickwedde.de>
2022-12-26 22:42:34 +0000

    x

--- main.js
+++ main.js
@@ -71,3 +71,46 @@ mqttclient.on('connect', () => {
   doModbus();
 })
 
+var SmartmeterObis = require('smartmeter-obis');
+
+var options = {
+'debug':0,
+    'protocol': "SmlProtocol",
+    'transport': "SerialResponseTransport",
+    'transportSerialPort': "/dev/ttyAMA0",
+    'transportSerialBaudrate': 9600,
+    'transportSerialDataBits': 8,
+    'transportSerialParity': 'none',
+    'transportSerialStopBits': 1,
+    'protocolD0WakeupCharacters': 40,
+    'protocolD0DeviceAddress': '',
+    'requestInterval': 10,
+    'obisNameLanguage': 'en',
+    'obisFallbackMedium': 6
+};
+
+async function displayData(err, obisResult) {
+    if (err) {
+        // handle error
+        // if you want to cancel the processing because of this error call smTransport.stop() before returning
+        // else processing continues
+        return;
+    }
+    await mqttclient.publish('modbus/0/power', JSON.stringify(obisResult["1-0:16.7.0*255"].valueToString()))
+    return;
+    for (var obisId in obisResult) {
+        console.log(
+            obisId,
+            obisResult[obisId].idToString() + ': ' +
+            SmartmeterObis.ObisNames.resolveObisName(obisResult[obisId], options.obisNameLanguage).obisName + ' = ' +
+            obisResult[obisId].valueToString()
+        );
+    }
+
+}
+
+var smTransport = SmartmeterObis.init(options, displayData);
+
+smTransport.process();
+
+
